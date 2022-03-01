c4fe0ae60758a17fa79ef53a85858aaeda56b37c
mosesvk <kaumatule93@gmail.com>
2022-03-01 10:13:09 -0700

    initial push

---
+++ public/text/brand.js
@@ -0,0 +1,13 @@
+module.exports = {
+  agency: {
+    name: 'PEM',
+    desc: 'PEM - Pure Enviro Management',
+    prefix: 'PEM',
+    footerText: 'MVK - All Rights Reserved 2021',
+    logoText: 'PEM',
+    projectName: 'Agency Theme',
+    url: 'luxireact.ux-maestro.com/agency',
+    img: '/static/images/PEM_logo.svg',
+    notifMsg: 'Donec sit amet nulla sed arcu pulvinar ultricies commodo id ligula.'
+  }
+};
